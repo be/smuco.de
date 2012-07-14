5bd1562b0475b4f8980859ea5cbf5115a108326a
Stig Murberg <stimurbe@cisco.com>
2012-07-14 02:29:37 +0200

    stay on smuco.de for internal links

--- _site/js/smucode.js
+++ _site/js/smucode.js
@@ -3,4 +3,12 @@
   for (var i = 0; i < anchors.length; i++) {
     anchors[i].setAttribute('target', '_top');
   }
+  if (window.location.hostname != 'localhost') {
+    for (var i = 0; i < anchors.length; i++) {
+      if (anchors[i].className == 'internal') {
+        var href = anchors[i].getAttribute('href');
+        anchors[i].setAttribute('href', 'http://smuco.de' + href);
+      }
+    }
+  }
 }());
