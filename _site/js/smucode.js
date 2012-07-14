6ac220c1b9824e62698ac495de8234d6b2f4f8ec
Stig Murberg <stimurbe@cisco.com>
2012-07-14 02:15:41 +0200

    added script to set target to top frame

--- _site/js/smucode.js
+++ _site/js/smucode.js
@@ -1,4 +1,6 @@
-var anchors = document.getElementsByTagName('a');
-for (var i = 0; i < anchors.length; i++) {
-  anchors[i].setAttribute('target', '_top');
-}
+(function() {
+  var anchors = document.getElementsByTagName('a');
+  for (var i = 0; i < anchors.length; i++) {
+    anchors[i].setAttribute('target', '_top');
+  }
+}());
